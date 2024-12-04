8c7fb9d6452403050b885e931295650c1a0e01e9
Dominick1998 <54410726+Dominick1998@users.noreply.github.com>
2024-12-04 14:17:50 -0600

    Create product.component.ts
    Angular component for managing and displaying products.

---
+++ frontend/src/app/components/product/product.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+  products = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  loadProducts() {
+    this.productService.getProducts().subscribe(data => {
+      this.products = data;
+    });
+  }
+}
