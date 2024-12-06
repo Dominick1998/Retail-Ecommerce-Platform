d71ed69173e1073a44789ce6581d93d76a097a2b
Dominick1998 <54410726+Dominick1998@users.noreply.github.com>
2024-12-05 21:24:27 -0600

    Create product-details.component.ts
    Displays detailed information about a product.

---
+++ frontend/src/app/components/product-details/product-details.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.scss']
+})
+export class ProductDetailsComponent implements OnInit {
+  product: any;
+
+  constructor(private route: ActivatedRoute, private productService: ProductService) {}
+
+  ngOnInit(): void {
+    const productId = this.route.snapshot.paramMap.get('id');
+    if (productId) {
+      this.productService.getProduct(productId).subscribe(data => {
+        this.product = data;
+      });
+    }
+  }
+}
