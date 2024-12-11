077b57e9cdf2a5313436b2902619f0ce8ce59cac
Dominick1998 <54410726+Dominick1998@users.noreply.github.com>
2024-12-11 09:53:43 -0600

    Create order-history.component.ts
    Fetches and displays the order history of the user.

---
+++ frontend/src/app/components/order-history/order-history.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-order-history',
+  templateUrl: './order-history.component.html',
+  styleUrls: ['./order-history.component.scss']
+})
+export class OrderHistoryComponent implements OnInit {
+  orders: any[] = [];
+  userId = JSON.parse(localStorage.getItem('user') || '{}').id;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadOrders();
+  }
+
+  loadOrders() {
+    this.http.get(`http://localhost:8080/api/orders/${this.userId}`).subscribe((data: any) => {
+      this.orders = data;
+    });
+  }
+}
