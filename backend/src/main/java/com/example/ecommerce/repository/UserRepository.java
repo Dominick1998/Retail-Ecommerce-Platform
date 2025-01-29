52a1679cd20cdb1e21330596ce806b0368ada8cf
Dominick1998 <54410726+Dominick1998@users.noreply.github.com>
2025-01-29 16:47:22 -0600

    Update UserRepository.java

--- backend/src/main/java/com/example/ecommerce/repository/UserRepository.java
+++ backend/src/main/java/com/example/ecommerce/repository/UserRepository.java
@@ -5,7 +5,6 @@ package com.example.ecommerce.repository;
 import com.example.ecommerce.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
