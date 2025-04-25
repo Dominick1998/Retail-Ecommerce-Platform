3bb456effd1f881994bf905257c8539681ef739b
Dominick1998 <54410726+Dominick1998@users.noreply.github.com>
2025-04-24 20:37:30 -0500

    Create RefreshTokenRepository.java

---
+++ backend/src/main/java/com/example/ecommerce/repository/RefreshTokenRepository.java
@@ -0,0 +1,6 @@
+package com.example.ecommerce.repository;
+
+import com.example.ecommerce.model.RefreshToken;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
