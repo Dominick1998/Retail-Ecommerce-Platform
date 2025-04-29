b970f6cd246af9cecff46fa0138baee1d474abb0
Dominick1998 <54410726+Dominick1998@users.noreply.github.com>
2025-04-29 15:35:38 -0500

    Create SwaggerConfig.java

---
+++ backend/src/main/java/com/example/ecommerce/config/SwaggerConfig.java
@@ -0,0 +1,8 @@
+package com.example.ecommerce.config;
+
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
