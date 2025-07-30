ab99b15039a7ebaeabc87f14a93e806a95876c4e
Dominick1998 <54410726+Dominick1998@users.noreply.github.com>
2025-07-30 13:51:41 -0500

    Update checkout.component.ts

--- frontend/src/app/components/checkout/checkout.component.ts
+++ frontend/src/app/components/checkout/checkout.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { loadStripe, Stripe } from '@stripe/stripe-js';
 import { PaymentService } from '../../services/payment.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-checkout',
